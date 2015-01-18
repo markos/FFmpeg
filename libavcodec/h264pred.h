6967e42bda7d880ce2b6272320ef465561a0c4de
Konstantinos Margaritis <markos@freevec.org>
2015-01-18 14:18:47 +0200

    Optimize H264 predicate functions with Altivec, ~6% gain

--- libavcodec/h264pred.h
+++ libavcodec/h264pred.h
@@ -119,5 +119,7 @@ void ff_h264_pred_init_x86(H264PredContext *h, int codec_id,
                            const int bit_depth, const int chroma_format_idc);
 void ff_h264_pred_init_mips(H264PredContext *h, int codec_id,
                             const int bit_depth, const int chroma_format_idc);
+void ff_h264_pred_init_ppc(H264PredContext *h, int codec_id,
+                           const int bit_depth, const int chroma_format_idc);
 
 #endif /* AVCODEC_H264PRED_H */
